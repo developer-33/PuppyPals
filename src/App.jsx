fbd34cc2942dfbfd92ebedaa9c83a7f8602a0e25
fletcher burton <burton.fletcher@aol.com>
2023-09-05 06:25:27 -0500

    commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import {puppyList} from './data.js'
+import './my.css'
+import './App.css'
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const [featuredPup, setFeaturedPup] = useState(null)
+
+  console.log(puppies)
+  return (
+    <>
+    <div>
+    {
+      puppies.map((puppy) => {
+        return <p onClick = { () => {
+          setFeaturedPup(puppy)
+          setFeatPupId(puppy.id)}
+      } key = {puppy.id} > {puppy.name} </p>
+      } )
+    }
+
+    {featuredPup && (
+    
+    <div>
+      <h1> Our Puppy Pals </h1>
+      <h1 className ={"my-pup"}> {featuredPup.name} </h1>
+    <ul> <h2> Click a Puppy to See Details </h2>
+      <li> Featured Puppy Id#: {featPupId}</li>
+      <li> Age: {featuredPup.age}</li>
+      <li> Email Address: {featuredPup.email}</li>
+      <li> Ownder ID#: {featuredPup.ownerId}</li>
+      <li> Number of tricks: {featuredPup.tricks.length}</li>
+    </ul>
+    </div>
+
+    )}
+    
+    </div>
+    </>
+  );
+
+  }
+
+  
+ 
+
+export default App
